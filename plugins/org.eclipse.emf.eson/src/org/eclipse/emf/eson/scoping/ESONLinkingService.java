670d4aa67fa4beb702c63a41df51be4e9d6d08fe
David Aguirre <daguirre@temenos.com>
2016-09-14 17:13:31 +0200

    [Fix] Stackoverflow exception with headless using new URI
    Change-Id: I425d47e6e1787a954406d945c4d44673c2333966
    Signed-off-by: David Aguirre <daguirre@temenos.com>

--- plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/scoping/ESONLinkingService.java
+++ plugins/org.eclipse.emf.eson/src/org/eclipse/emf/eson/scoping/ESONLinkingService.java
@@ -19,25 +19,16 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.eson.eFactory.EFactoryPackage;
 import org.eclipse.emf.eson.eFactory.PackageImport;
 import org.eclipse.emf.eson.util.EPackageRegistry;
 import org.eclipse.xtext.linking.impl.DefaultLinkingService;
 import org.eclipse.xtext.linking.impl.IllegalNodeException;
-import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.nodemodel.INode;
-import org.eclipse.xtext.resource.IEObjectDescription;
-import org.eclipse.xtext.resource.IResourceDescriptions;
-import org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider;
-import org.eclipse.xtext.scoping.IGlobalScopeProvider;
-import org.eclipse.xtext.scoping.IScope;
 
 import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
 import com.google.inject.Inject;
 
 /**
@@ -51,9 +42,6 @@ import com.google.inject.Inject;
 public class ESONLinkingService extends DefaultLinkingService {
 
 	private static final Logger log = Logger.getLogger(ESONLinkingService.class);
-
-	private @Inject IGlobalScopeProvider scopeProvider;
-	private @Inject ResourceDescriptionsProvider descriptionsProvider;
 	private @Inject EPackageRegistry ePackageRegistry;
 	
 	@Override
@@ -63,54 +51,16 @@ public class ESONLinkingService extends DefaultLinkingService {
 		return super.getLinkedObjects(context, ref, node);
 	}
 
-	// Following code is (way too much..) copy/pasted (almost) verbatim/as-is from XtextLinkingService
-	// Proposed to Xtext to factor out re-usable code not related to xtext.xtext,
-	// but of interest to any langs dealing with EPackage, via bug/issue
-	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=489748.
-	
+	// Code below was based on XtextLinkingService, which was not too nice and lead
+	// to https://bugs.eclipse.org/bugs/show_bug.cgi?id=489748.
+	// Old implementation was causing StackOverflow issues
 	private List<EObject> getPackage(PackageImport context, String nsUri) {
 		if (nsUri == null)
 			return Collections.emptyList();
-		QualifiedName packageNsURI = QualifiedName.create(nsUri);
-		EPackage pack = findPackageInScope(context, packageNsURI);
-		if (pack == null) {
-			pack = findPackageInAllDescriptions(context, packageNsURI);
-			if (pack == null) {
-				pack = loadEPackage(nsUri, context.eResource().getResourceSet());
-			}
-		}
+		EPackage pack = loadEPackage(nsUri, context.eResource().getResourceSet());
 		if (pack != null)
 			return Collections.<EObject>singletonList(pack);
 		return Collections.emptyList();
-	}
-
-	private EPackage findPackageInScope(EObject context, QualifiedName packageNsURI) {
-		IScope scopedPackages = scopeProvider.getScope(context.eResource(), EFactoryPackage.Literals.PACKAGE_IMPORT__EPACKAGE, new Predicate<IEObjectDescription>() {
-			@Override
-			public boolean apply(IEObjectDescription input) {
-				return isNsUriIndexEntry(input);
-			}
-		});
-		IEObjectDescription description = scopedPackages.getSingleElement(packageNsURI);
-		if (description != null) {
-			return getResolvedEPackage(description, context);
-		}
-		return null;
-	}
-	
-	private EPackage findPackageInAllDescriptions(EObject context, QualifiedName packageNsURI) {
-		IResourceDescriptions descriptions = descriptionsProvider.getResourceDescriptions(context.eResource());
-		if (descriptions != null) {
-			Iterable<IEObjectDescription> exported = descriptions.getExportedObjects(EcorePackage.Literals.EPACKAGE, packageNsURI, false);
-			for(IEObjectDescription candidate: exported) {
-				if (isNsUriIndexEntry(candidate)) {
-					EPackage result = getResolvedEPackage(candidate, context);
-					if (result != null)
-						return result;
-				}
-			}
-		}
-		return null;
 	}
 
 	private EPackage loadEPackage(String resourceOrNsURI, ResourceSet resourceSet) {
@@ -140,17 +90,6 @@ public class ESONLinkingService extends DefaultLinkingService {
 			log.trace("Cannot load package with URI '" + resourceOrNsURI + "'", ex);
 			return null;
 		}
-	}
-
-	private EPackage getResolvedEPackage(IEObjectDescription description, EObject context) {
-		EObject resolved = EcoreUtil.resolve(description.getEObjectOrProxy(), context);
-		if (resolved != null && !resolved.eIsProxy() && resolved instanceof EPackage)
-			return (EPackage) resolved;
-		return null;
-	}
-	
-	private boolean isNsUriIndexEntry(IEObjectDescription candidate) {
-		return Boolean.TRUE.toString().equalsIgnoreCase(candidate.getUserData("nsURI"));
 	}
 
 }
